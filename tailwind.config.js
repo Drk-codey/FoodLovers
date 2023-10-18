434aa96c25a244e433111d019bd8af4d5fc8a544
Drk~Codey <109389152+Drk-codey@users.noreply.github.com>
2023-10-18 01:30:49 +0100

    Add new themes extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {},
+      colors: {
+        bgColor: '#FCEEFD',
+        pinkBg: '#FDF5FE',
+        primary: '#EF5DA8',
+        textGray: '#B2B2B2',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'body': ['Poppins'],
+      },
+      backgroundImage: {
+        bgGradient: 'linear-gradient(0deg, #FCEEFD 0%, rgba(252, 238, 253, 0.00) 100%)',
+      },
+      boxShadow: {
+        outsideBox: '0px 10px 30px 0px rgba(57, 57, 57, 0.05)'
+      },
+    },
   },
   plugins: [],
-}
-
+};
